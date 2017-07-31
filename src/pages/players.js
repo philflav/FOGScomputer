8b9207c6cee459b564f44951b07f4efbf795ca2b
Phil <pflavin@gmail.com>
2017-07-31 11:24:03 +0100

    Scorecard rendering working. Points calculation not yet included

--- src/pages/players.js
+++ src/pages/players.js
@@ -12,8 +12,33 @@ var dbRefComps = fire.database().ref().child('comp');
 var dbRefScorecards = fire.database().ref().child('scorecard');
 var dbRefCourses = fire.database().ref().child('course');
 
+var day1f9score=[];
+var day1f9total;
+var day1b9score=[];
+var day1b9total;
+var day1total;
+var day1hcap;
 
- 
+var day2f9score=[];
+var day2f9total;
+var day2b9score=[];
+var day2b9total;
+var day2total;
+var day2hcap;
+
+var day3f9score=[];
+var day3f9total;
+var day3b9score=[];
+var day3b9total;
+var day3total;
+var day3hcap;
+
+var day4f9score=[];
+var day4f9total;
+var day4b9score=[];
+var day4b9total;
+var day4total;
+var day4hcap;
 export default class Players  extends React.Component {
 
      
@@ -59,19 +84,110 @@ export default class Players  extends React.Component {
                 
                    //Lookup course ID for courses played     
                 dbRefScorecard.once('value').then(snap => {
+
                 snap.forEach((child) =>{
                     if((this.state.index1)===child.val().scorecard_id){
-                        this.setState({courseId1: child.val().course_id})                       
+                        this.setState({courseId1: child.val().course_id}) 
+                        day1f9score[1] =  child.val().S1
+                        day1f9score[2] =  child.val().S2 
+                        day1f9score[3] =  child.val().S3 
+                        day1f9score[4] =  child.val().S4 
+                        day1f9score[5] =  child.val().S5 
+                        day1f9score[6] =  child.val().S6 
+                        day1f9score[7] =  child.val().S7 
+                        day1f9score[8] =  child.val().S8 
+                        day1f9score[9] =  child.val().S9 
+                        day1f9total = day1f9score.reduce((x, y) => x + y); 
+                        day1b9score[1] =  child.val().S10
+                        day1b9score[2] =  child.val().S11 
+                        day1b9score[3] =  child.val().S12 
+                        day1b9score[4] =  child.val().S13 
+                        day1b9score[5] =  child.val().S14 
+                        day1b9score[6] =  child.val().S15 
+                        day1b9score[7] =  child.val().S16 
+                        day1b9score[8] =  child.val().S17 
+                        day1b9score[9] =  child.val().S18 
+                        day1b9total = day1b9score.reduce((x, y) => x + y);
+                        day1total = day1f9total+day1b9total 
+                        day1hcap =  child.val().hcap
+                                       
                     }
                     if((this.state.index2)===child.val().scorecard_id){
-                        this.setState({courseId2: child.val().course_id})                       
+                        this.setState({courseId2: child.val().course_id})
+                        day2f9score[1] =  child.val().S1
+                        day2f9score[2] =  child.val().S2 
+                        day2f9score[3] =  child.val().S3 
+                        day2f9score[4] =  child.val().S4 
+                        day2f9score[5] =  child.val().S5 
+                        day2f9score[6] =  child.val().S6 
+                        day2f9score[7] =  child.val().S7 
+                        day2f9score[8] =  child.val().S8 
+                        day2f9score[9] =  child.val().S9 
+                        day2f9total = day2f9score.reduce((x, y) => x + y); 
+                        day2b9score[1] =  child.val().S10
+                        day2b9score[2] =  child.val().S11 
+                        day2b9score[3] =  child.val().S12 
+                        day2b9score[4] =  child.val().S13 
+                        day2b9score[5] =  child.val().S14 
+                        day2b9score[6] =  child.val().S15 
+                        day2b9score[7] =  child.val().S16 
+                        day2b9score[8] =  child.val().S17 
+                        day2b9score[9] =  child.val().S18 
+                        day2b9total = day2b9score.reduce((x, y) => x + y);
+                        day2total = day2f9total+day2b9total 
+                        day2hcap =  child.val().hcap                      
                     }
                     if((this.state.index3)===child.val().scorecard_id){
-                        this.setState({courseId3: child.val().course_id})                       
+                        this.setState({courseId3: child.val().course_id})
+                        day3f9score[1] =  child.val().S1
+                        day3f9score[2] =  child.val().S2 
+                        day3f9score[3] =  child.val().S3 
+                        day3f9score[4] =  child.val().S4 
+                        day3f9score[5] =  child.val().S5 
+                        day3f9score[6] =  child.val().S6 
+                        day3f9score[7] =  child.val().S7 
+                        day3f9score[8] =  child.val().S8 
+                        day3f9score[9] =  child.val().S9 
+                        day3f9total = day3f9score.reduce((x, y) => x + y); 
+                        day3b9score[1] =  child.val().S10
+                        day3b9score[2] =  child.val().S11 
+                        day3b9score[3] =  child.val().S12 
+                        day3b9score[4] =  child.val().S13 
+                        day3b9score[5] =  child.val().S14 
+                        day3b9score[6] =  child.val().S15 
+                        day3b9score[7] =  child.val().S16 
+                        day3b9score[8] =  child.val().S17 
+                        day3b9score[9] =  child.val().S18 
+                        day3b9total = day3b9score.reduce((x, y) => x + y);
+                        day3total = day3f9total+day3b9total 
+                        day3hcap =  child.val().hcap                          
                     }
                     if((this.state.index4)===child.val().scorecard_id){
-                        this.setState({courseId4: child.val().course_id})                       
+                        this.setState({courseId4: child.val().course_id}) 
+                        day4f9score[1] =  child.val().S1
+                        day4f9score[2] =  child.val().S2 
+                        day4f9score[3] =  child.val().S3 
+                        day4f9score[4] =  child.val().S4 
+                        day4f9score[5] =  child.val().S5 
+                        day4f9score[6] =  child.val().S6 
+                        day4f9score[7] =  child.val().S7 
+                        day4f9score[8] =  child.val().S8 
+                        day4f9score[9] =  child.val().S9 
+                        day4f9total = day4f9score.reduce((x, y) => x + y); 
+                        day4b9score[1] =  child.val().S10
+                        day4b9score[2] =  child.val().S11 
+                        day4b9score[3] =  child.val().S12 
+                        day4b9score[4] =  child.val().S13 
+                        day4b9score[5] =  child.val().S14 
+                        day4b9score[6] =  child.val().S15 
+                        day4b9score[7] =  child.val().S16 
+                        day4b9score[8] =  child.val().S17 
+                        day4b9score[9] =  child.val().S18 
+                        day4b9total = day4b9score.reduce((x, y) => x + y);
+                        day4total = day4f9total+day4b9total 
+                        day4hcap =  child.val().hcap                      
                     }
+                    
                      //console.log("1",this.state)
                      
                     })
@@ -114,10 +230,10 @@ export default class Players  extends React.Component {
                 <Player forename = {this.state.forename} surname = {this.state.surname} handicap = {this.state.handicap} email = {this.state.email} phone = {this.state.phone}/>          
                        
                         <div>
-                        <Scorecard index={this.state.courseId1} courseName = {this.state.courseName1} /> ;
-                        <Scorecard courseName = {this.state.courseName2} /> ;
-                        <Scorecard courseName = {this.state.courseName3} /> ;
-                        <Scorecard courseName = {this.state.courseName4} /> ;
+                        <Scorecard courseName = {this.state.courseName1} handicap={day1hcap} scores= {day1f9score.concat(day1b9score)} f9={day1f9total} b9={day1b9total} total={day1total}/> ;
+                        <Scorecard courseName = {this.state.courseName2}  handicap={day2hcap} scores= {day2f9score.concat(day2b9score)} f9={day2f9total} b9={day2b9total} total={day2total}/>;
+                        <Scorecard courseName = {this.state.courseName3}  handicap={day3hcap} scores= {day3f9score.concat(day3b9score)} f9={day3f9total} b9={day3b9total} total={day3total}/> ;
+                        <Scorecard courseName = {this.state.courseName4}  handicap={day4hcap} scores= {day4f9score.concat(day4b9score)} f9={day4f9total} b9={day4b9total} total={day4total}/> ;
                         </div>
                       
             </div>
--- src/pages/scorecard.js
+++ src/pages/scorecard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Table} from 'react-bootstrap'
 //import PropTypes from 'prop-types';
 
 
@@ -6,9 +7,14 @@ import React, { Component } from 'react';
 
 
 export class Scorecard extends Component {
-        
 
-    componentWillMount() {}
+     
+
+    componentWillMount() {
+
+
+
+    }
 
 
     componentDidMount() {
@@ -43,12 +49,159 @@ export class Scorecard extends Component {
     }
 
     render() {
-
         
-
         return (
             <div>
-                <h1>Scorecard for {this.props.courseName}  </h1>
+                <h5>Scorecard for {this.props.courseName}. Handicap {this.props.handicap} </h5>
+                <div>
+                <Table responsive cols="6">
+                <thead>
+                <tr>
+                    <th>Hole</th>  
+                    <th> 1</th>
+                    <th> 2</th>
+                    <th> 3</th>
+                    <th> 4</th>
+                    <th> 5</th>
+                    <th> 6</th>
+                    <th> 7</th>
+                    <th> 8</th>
+                    <th> 9</th>
+                    <th>F9</th>
+                    <th>   </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Par</td>  
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+
+                </tr>
+                <tr>
+                    <td>SI</td> 
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>{this.props.scores[1]}</td>
+                    <td>{this.props.scores[2]}</td>
+                    <td>{this.props.scores[3]}</td>
+                    <td>{this.props.scores[4]}</td>
+                    <td>{this.props.scores[5]}</td>
+                    <td>{this.props.scores[6]}</td>
+                    <td>{this.props.scores[6]}</td>
+                    <td>{this.props.scores[8]}</td>
+                    <td>{this.props.scores[9]}</td>
+                    <td>{this.props.f9}</td>
+                </tr>
+                <tr>
+                    <td>Pts</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+ 
+                <thead>
+                <tr>
+                    <th>Hole</th>  
+                    <th>10</th>
+                    <th>11</th>
+                    <th>12</th>
+                    <th>13</th>
+                    <th>14</th>
+                    <th>15</th>
+                    <th>16</th>
+                    <th>17</th>
+                    <th>18</th>
+                    <th>B9</th>
+                    <th>Total</th>
+                  
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Par</td>  
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4</td>
+
+                </tr>
+                <tr>
+                    <td>SI</td> 
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>{this.props.scores[10+1]}</td>
+                    <td>{this.props.scores[11+1]}</td>
+                    <td>{this.props.scores[12+1]}</td>
+                    <td>{this.props.scores[13+1]}</td>
+                    <td>{this.props.scores[14+1]}</td>
+                    <td>{this.props.scores[15+1]}</td>
+                    <td>{this.props.scores[16+1]}</td>
+                    <td>{this.props.scores[17+1]}</td>
+                    <td>{this.props.scores[18+1]}</td>
+                    <td>{this.props.b9}</td>
+                    <td>{this.props.total}</td>
+  
+                </tr>
+                <tr>
+                    <td>Pts</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+                </Table>
+                </div>
+                
             </div>
         );
     }
