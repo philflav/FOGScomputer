7f4bbb33cb7ec2377b9d1d8e953eacf0b837ef41
Phil <pflavin@gmail.com>
2017-08-30 11:24:28 +0100

    Basic Draw functionality implemented in featured.js

--- src/pages/featured.js
+++ src/pages/featured.js
@@ -1,56 +1,48 @@
 import React from "react"; 
 
-import Hole from './hole.js'
+import {Panel, Button} from 'react-bootstrap'
  
-var holeData =[]
 
-//This js is being used as a component test only.
 export default class Featured extends React.Component {
-    constructor(props){
-        super(props)
 
-        this.state = {test: 'Goodbye',
-                      number: 1,
-                      par: 4,
-                      SI: 17,
-                      key: 33}
+    handleClick (i,event) {
+        console.log(i)
+
+        var selectedButton = document.getElementById(i);
+        selectedButton.setAttribute('disabled', true)
+
+        alert(i)        
     }
 
-    handlechange(event) {
-        const target = event.target
-        const name = target.name
-        const value = target.value
-        console.log(target)
-        this.setState({
-           
-            [name]: value});
-       
-
-    }
-        
-
-    saveData(event){
-        event.preventDefault()
-        console.log('Saving :',this.state.key, this.state.SI, this.state.par)
-        for(var i=0;i<10;i++){
-            holeData.push(<Hole number={i}  hkey={this.state.key} par={this.state.par} SI={this.state.SI}/>)
-            }
-    }
 
     
     render () {
+        var nums = [1,2,3,4,5,6,7,8,9,10,11,12]
+        var ranNums = []
+        var i = nums.length
+        var j = 0
+    
+            while (i--) {
+                j = Math.floor(Math.random() * (i+1));
+                ranNums.push(nums[j]);
+                nums.splice(j,1);
+    }
+        var items = ranNums.map(function(i) {
+            return (
+               
+                <Button id={i} bsStyle="success" onClick={this.handleClick.bind(this, i)} >
+                  ????
+                </Button>
+              
+                
+                );
+        }, this);
 
         return (
             <div>
-                  <form onSubmit ={this.saveData.bind(this)}>
-                    <td>{this.state.number+1} </td>                             
-                    <td><input style = {{"width":"100px"}} ref={this.state.key} name="par" value={this.state.par} onChange={this.handlechange.bind(this)}/> </td>
-                    <td><input style = {{"width":"100px"}} ref={this.state.key} name="SI" value={this.state.SI} onChange={this.handlechange.bind(this)}/> </td>
-                    <td>
-                    <button type="submit">  Save</button>
-                    </td>
-                    </form>   
-                    {holeData}    
+                 <Panel header="The Draw">
+                 {items}
+                 </Panel>
 
             </div>
 
