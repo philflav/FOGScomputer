abbbb1b0e86216374b6c90e3573c2c04473ad3de
Phil <pflavin@gmail.com>
2017-09-01 17:00:50 +0100

    Draw functionality now largely working. Still need to fisx disabling of player select when player has been drawn

--- src/pages/draw.js
+++ src/pages/draw.js
@@ -8,20 +8,43 @@ var dbRefComps = fire.database().ref().child('comp');
 var dbRefPlayers = fire.database().ref().child('player');
 
 var selCompName = ''
+var selPlayer
 var players = [] //array of competition record objects
 var playerCount=1
+var buttonItems
 var link
 const maxPlayers =12 //set the maximum nuber of players allowed
 
 export default class Draw extends React.Component {
     constructor(props){
         super(props)
-        this.state ={players: []}
+        this.state ={players: [],
+        selPlayerName: 'no one'}
+        
         selCompName='The draw for '+this.props.match.params.compName
 
         link='/'+this.props.match.params.compName
-    }
+ 
+    //create buttons
+    var nums = new Array(maxPlayers).join().split(',').map(function(item, index){ return ++index;})//fills an array of integers
+    var ranNums = []
+    var i = maxPlayers
+    var j = 0
 
+        while (i--) {
+            j = Math.floor(Math.random() * (i+1));
+            ranNums.push(nums[j]);
+            nums.splice(j,1);
+        }
+       buttonItems = ranNums.map(function(i) {
+        return (           
+            <Button id={i} bsStyle="success" bsSize="large" onClick={this.handleButtonClick.bind(this, i)} block >
+              ????
+            </Button>                     
+            );    }, this)
+       
+        }
+  
     componentWillMount(){
 
         //get competion players
@@ -34,20 +57,23 @@ export default class Draw extends React.Component {
                         var dbRefPlayer= dbRefPlayers.orderByChild('player_id').equalTo(child.val().player_id);
                         dbRefPlayer.once('value').then(snap =>{
                             snap.forEach((child1) => {
-                                players.push({competitorRef: child.ref, player_id: child.val().player_id, draw: child.val().draw, playerName: child1.val().forename+' '+child1.val().surname})
-                                if(child.val().draw){
+                                players.push({competitorKey: child.key, player_id: child.val().player_id, draw: child.val().draw, playerName: child1.val().forename+' '+child1.val().surname})
+                                if(child.val().draw>0){
                                 var selectedButton = document.getElementById(child.val().draw);
                                 selectedButton.setAttribute('disabled', true)
                                 selectedButton.innerHTML= child1.val().forename +' '+ child1.val().surname+' drew '+ child.val().draw
                                 }
-                                })
-                                this.setState({players: players})
+                                })  
+                                
+                            this.setState({players: players})                             
                             })
+                            
                         })
                         for(var j=playerCount+1;j<maxPlayers+1;j++){
                         var selectedButton = document.getElementById(j)
                         selectedButton.remove()
                     }
+
                     })                 
                     console.log(players)
                 
@@ -59,60 +85,59 @@ export default class Draw extends React.Component {
 
     }
 
-    handleClick (i,event) {  //draw button clicked
+    handleButtonClick (i,event) {  //draw button clicked
         var selectedButton = document.getElementById(i);
         selectedButton.setAttribute('disabled', true)
-        selectedButton.innerHTML= i + ' not drawn'   
+        var update ={draw: i}
+        console.log(selPlayer.competitorKey, update)
+        fire.database().ref('/comp/'+selPlayer.competitorKey).update(update)
+        .catch(e =>{
+         console.log(e)
+        })
+        .then(
+            selectedButton.innerHTML= selPlayer.playerName + ' drew '+ i,
+            this.setState({selPlayerName: 'no one'}),
+            selPlayer.competitorKey='',
+            
+        )
+
+
     }
 
-    handleSelect(eventKey){  //player selected
+    handleMenuSelect(eventKey){  //comp database reference to player selected
 
-        alert(eventKey)
+        selPlayer = players[eventKey]
+        console.log(selPlayer)
+        this.setState({selPlayerName: players[eventKey].playerName})
+
+
 
     }
-
 
     
     render () {
-        var nums = new Array(maxPlayers).join().split(',').map(function(item, index){ return ++index;})//fills an array of integers
-        var ranNums = []
-        var i = maxPlayers
-        var j = 0
-    
-            while (i--) {
-                j = Math.floor(Math.random() * (i+1));
-                ranNums.push(nums[j]);
-                nums.splice(j,1);
-    }
-        var items = ranNums.map(function(i) {
-            return (
-               
-                <Button id={i} bsStyle="success" bsSize="large" onClick={this.handleClick.bind(this, i)} block >
-                  ????
-                </Button>
-              
-                
-                );
-        }, this)
-
-        var menuItems
+        var menuItems =[]
         var plist = []
         plist=this.state.players
-      
-        menuItems = plist.map((player) => (
-        <MenuItem eventKey={player.player_id}>{player.player_id} {player.playerName} </MenuItem>))
+        menuItems = plist.map((player, index) => {
+            var mindex='M'+index
+            if(!player.draw){
+                return  <MenuItem id={mindex} eventKey={index}>{player.playerName} </MenuItem>
+            }
+        })
 
         return (
             <div>
                  <Panel bsStyle="primary" header= {selCompName}>
-                 <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
+                     <h2>Drawing {this.state.selPlayerName}</h2>
+                 <Nav bsStyle="tabs" activeKey="1" >
                      <NavItem href={link}> Teams/Groups </NavItem>
-                    <NavDropdown eventKey="999" title="Select Player to Draw" id="nav-dropdown">
+                    <NavDropdown onSelect={this.handleMenuSelect.bind(this)} eventKey="999" title="Select Player to Draw" id="nav-dropdown">
                         {menuItems}
                     </NavDropdown>
                 </Nav>
                  <Well>
-                 {items}
+                 {buttonItems}
                  </Well>
                  </Panel>
 
