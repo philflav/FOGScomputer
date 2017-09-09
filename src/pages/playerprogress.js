bd6af2b546040d1741d087d0624799fb7e12f3ec
Phil <pflavin@gmail.com>
2017-09-09 15:47:57 +0100

    Player progress display working and initial styling completed.

---
+++ src/pages/playerprogress.js
@@ -0,0 +1,48 @@
+import React from "react"; 
+
+import {Panel, Well, Glyphicon} from 'react-bootstrap'
+
+var holesPlayer
+var runningTotal
+var playerName
+
+
+export default class PlayerProgress extends React.Component {
+
+    constructor(props){
+        super(props)
+        holesPlayer=this.props.holes
+        runningTotal=this.props.total
+        playerName=this.props.name
+    }
+
+    componentWillReceiveProps(nextProps){
+
+        holesPlayer=nextProps.holes
+        runningTotal = nextProps.total
+        playerName = nextProps.name
+    }
+
+render () {
+
+   var rightStyle = {
+        textAlign: 'right',
+        paddingRight: '10px'
+    }
+    var leftStyle = {
+        textAlign: 'left',
+        paddingLeft: '12px',
+        paddingRight: '10px'
+    }
+
+     return (        
+      
+         <tr>
+         <td style={rightStyle}>{playerName}</td><td style={leftStyle}><Glyphicon glyph="flag" />{holesPlayer}</td><td style={leftStyle}> <Glyphicon glyph="arrow-right" />{runningTotal} pts</td>
+         </tr>
+        
+    )
+}
+
+    
+    }
